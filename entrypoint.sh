aad0f8fcd8b9c526c6f9016be34fa75db1638822
victornavelino <victornavelino@gmail.com>
2023-07-06 20:31:01 -0300

    sdsd

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ python /opt/carniceriavv/manage.py migrate
 
 python /opt/carniceriavv/manage.py collectstatic
 
-#python /opt/carniceriavv/manage.py runserver 0.0.0.0:8000
+python /opt/carniceriavv/manage.py runserver 0.0.0.0:8000
