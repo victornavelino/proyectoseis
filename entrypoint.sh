155df04726fb98512b26d7beb8e8d2d51b418804
victornavelino <victornavelino@gmail.com>
2023-07-06 20:08:42 -0300

    fdfddf

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "${0}: running migrations."
-python /opt/carniceriavv/manage.py makemigrations 
-python /opt/carniceriavv/manage.py migrate
+python manage.py makemigrations 
+python manage.py migrate
 
 echo "${0}: collecting statics."
 
-python /opt/carniceriavv/manage.py collectstatic
+python manage.py collectstatic
 
-python /opt/carniceriavv/manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
