e7486f3835bf4a3bf37c614519d559221b36e091
hugo <victornavelino@gmail.com>
2023-08-14 19:14:02 -0300

    wewew

--- project/assets/promocion/formset_handlers.js
+++ project/assets/promocion/formset_handlers.js
@@ -24,4 +24,32 @@ jQuery("#id_es_por_precio").click(function(event) {
         jQuery("#id_promocionarticulo_set-0-valor").prop("disabled", true);
     }
 
+
+    $(document).ready(function() {
+        // Escuchar el clic en el botón "Agregar otro"
+        $(document).on("click", ".add-row a", function() {
+          // Esperar un breve momento para asegurarnos de que el nuevo componente se haya agregado
+          setTimeout(function() {
+            // Obtener el índice del nuevo componente agregado
+            var newIndex = $(".dynamic-tabular.inline-group").find(".form-row").length - 1;
+      
+            // Construir el ID del nuevo componente
+            var newComponentId = "#id_libro_set-" + newIndex + "-titulo";
+      
+            // Acceder al componente y obtener su ID
+            var idDelNuevoComponente = $(newComponentId).attr("id");
+      
+            console.log("ID del nuevo componente:", idDelNuevoComponente);
+          }, 100); // Ajustar este tiempo según sea necesario
+        });
+      });
+
+
+
+
+
+
+
+
+
 });
