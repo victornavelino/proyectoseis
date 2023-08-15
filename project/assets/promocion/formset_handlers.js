c1edc1a044b8a3eae78938f2ba69d01a049b690f
victornavelino <victornavelino@gmail.com>
2023-08-14 23:18:01 -0300

    ssdss

--- project/assets/promocion/formset_handlers.js
+++ project/assets/promocion/formset_handlers.js
@@ -1,55 +1,46 @@
-
+//no inicia chequeado. asi que desativamos el campo de precio
 if (!jQuery("#id_es_por_precio").is(":checked")) {
     jQuery("#id_promocionarticulo_set-0-valor").val(0)
     jQuery("#id_promocionarticulo_set-0-valor").prop("disabled", true);
 }
 
-
-jQuery("#id_es_por_precio").click(function(event) {
+//evento asociado al check
+jQuery("#id_es_por_precio").click(function (event) {
     if (jQuery("#id_es_por_precio").is(":checked")) {
         jQuery("#id_promocionarticulo_set-0-valor").val(null)
         jQuery("#id_promocionarticulo_set-0-valor").prop("disabled", false);
-    }else{
+    } else {
         jQuery("#id_promocionarticulo_set-0-valor").val(0)
         jQuery("#id_promocionarticulo_set-0-valor").prop("disabled", true);
     }
- });
+});
 
- document.addEventListener('formset:added', (event) => {
-    if (jQuery("#id_es_por_precio").is(":checked")) {
-        jQuery("#id_promocionarticulo_set-0-valor").val(null)
-        jQuery("#id_promocionarticulo_set-0-valor").prop("disabled", false);
-    }else{
-        jQuery("#id_promocionarticulo_set-0-valor").val(0)
-        jQuery("#id_promocionarticulo_set-0-valor").prop("disabled", true);
-    }
-
-
-    $(document).ready(function() {
-        // Escuchar el clic en el botón "Agregar otro"
-        $(document).on("click", ".add-row a", function() {
-          // Esperar un breve momento para asegurarnos de que el nuevo componente se haya agregado
-          setTimeout(function() {
+jQuery(document).ready(function () {
+    let newComponentId = null
+    // Escuchar el clic en el botón "Agregar otro"
+    jQuery(document).on("click", ".add-row a", function () {
+        // Esperar un breve momento para asegurarnos de que el nuevo componente se haya agregado
+        setTimeout(function () {
             // Obtener el índice del nuevo componente agregado
-            var newIndex = $(".dynamic-tabular.inline-group").find(".form-row").length - 1;
-      
+            var newIndex = jQuery(".js-inline-admin-formset").find(".form-row").length - 2;
+            console.log('nuevo index');
+            console.log(newIndex);
             // Construir el ID del nuevo componente
-            var newComponentId = "#id_libro_set-" + newIndex + "-titulo";
-      
+            newComponentId = "#id_promocionarticulo_set-" + newIndex + "-valor";
+            console.log(newComponentId)
             // Acceder al componente y obtener su ID
-            var idDelNuevoComponente = $(newComponentId).attr("id");
-      
+            var idDelNuevoComponente = jQuery(newComponentId).attr("id");
+
             console.log("ID del nuevo componente:", idDelNuevoComponente);
-          }, 100); // Ajustar este tiempo según sea necesario
-        });
-      });
+            //ahora ya con el componente obtenido vemos si lo habilitamos o no
+            if (jQuery("#id_es_por_precio").is(":checked")) {
+                jQuery(newComponentId).val(null)
+                jQuery(newComponentId).prop("disabled", false);
+            } else {
+                jQuery(newComponentId).val(0)
+                jQuery(newComponentId).prop("disabled", true);
+            }
+        }, 100); // Ajustar este tiempo según sea necesario
 
-
-
-
-
-
-
-
-
+    });
 });
